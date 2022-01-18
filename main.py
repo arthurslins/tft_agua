d7b877db7f48fc05d943cfe6a929658e8182f65b
Arthur Lins <arthurlins@id.uff.br>
2022-01-18 01:16:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -160,7 +160,7 @@ def main ():
 
             snap3=snap3.merge(parcial,how="left",on="Nick")
             
-            snap3.iloc[:,1:-2]=snap3.iloc[:,1:-2].astype(int)
+            snap3.iloc[:,1:-3]=snap3.iloc[:,1:-3].astype(int)
             
             snap3["Partidas Totais no snap"]=abs(snap3["Jogos Totais_y"]-snap3["Partidas Totais_x"])
             

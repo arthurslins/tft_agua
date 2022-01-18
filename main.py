8a2f8dcbf8567f82e4c76770f83503d5b7f04d9f
Arthur Lins <arthurlins@id.uff.br>
2022-01-18 00:59:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -160,7 +160,7 @@ def main ():
 
             snap3=snap3.merge(parcial,how="left",on="Nick")
             # st.write(snap3)
-            snap3["Partidas Totais no snap"]=abs(snap3["Jogos Totais"]-snap3["Partidas Totais"])
+            snap3["Partidas Totais no snap"]=abs(snap3["Jogos Totais"]-parcial["Partidas Totais"])
             snap3.sort_values(by="League Points",ascending=False,inplace=True)
             snap3=snap3.fillna(0)
             # st.write(snap3)
